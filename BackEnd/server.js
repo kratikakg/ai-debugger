f224123debd510c5a1b1141b5e88b5df765cc160
kratikakg <132834070+kratikakg@users.noreply.github.com>
2025-03-15 21:54:25 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app');
-
+const cors=require('cors');
 const PORT = process.env.PORT || 3000;  
-
+app.use(cors({
+    origin: "https://aidebugger.vercel.app",
+}));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
