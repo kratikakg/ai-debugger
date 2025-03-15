0b454eacd4df2b6ab9b6c947bbed8fe08678e5fd
kratikakg <132834070+kratikakg@users.noreply.github.com>
2025-03-15 19:13:03 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,15 +2,19 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors({
-    origin: "*", 
+    origin: "https://aidebugger.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
-
+app.options("*", cors());
 
 app.use(express.json());
+
+const aiRoutes = require("./routes/ai.routes");
+app.use("/ai", aiRoutes);
 
 module.exports = app;
