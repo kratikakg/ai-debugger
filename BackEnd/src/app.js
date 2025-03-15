8f25b06529e190198dd0c706f3825e56ba220be4
kratikakg <132834070+kratikakg@users.noreply.github.com>
2025-03-15 18:16:33 +0530

    axios bug fixes

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const aiRoutes = require('./routes/ai.routes');
-// const cors= require('cors');
+const aiRoutes = require('./routes/ai.routes')
+const cors = require('cors')
 
-const app= express()
-// app.use(cors());
-app.use(express.json());
+const app = express()
 
+app.use(cors({ origin: "http://localhost:5173" }));
+
+
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.send('Hello World')
+})
 
-app.use('/ai', aiRoutes);
-module.exports = app;
+app.use('/ai', aiRoutes)
+
+module.exports = app
